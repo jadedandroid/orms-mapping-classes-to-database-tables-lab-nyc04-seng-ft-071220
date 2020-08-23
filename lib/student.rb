ff2e7136f9db31e8f558f1dbc6209401db08e2ad
jade decordova <lacroixjaded@gmail.com>
2020-08-23 02:40:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,33 @@
 class Student
   
-  attr_accessor :name, :grade, :id
+  attr_accessor :name, :grade,
+  attr_reader :id
   
   def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
 
+  end
+  
+  def self.create_table
+    
+    
+  end
+  
+  def self.drop_table
+    
+    
+    
+  end
+  
+  def save
+    
+    
+  end
+  
+  def self.create
+    
+    
   end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
